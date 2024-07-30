133ae964eb21abc78e402307c689021b012f881c
Jesse P. Johnson <jpj6652@gmail.com>
2024-07-30 18:40:01 -0500

    docs: update project docstring

--- src/example_pipeline/__init__.py
+++ src/example_pipeline/__init__.py
@@ -1,6 +1,8 @@
 # SPDX-FileCopyrightText: © 2024 Jesse P. Johnson <jpj6652@gmail.com>
 # SPDX-License-Identifier: Unlicensed
 
+"""Provide example pipelines to prototype github actions."""
+
 import logging
 from typing import List
 
